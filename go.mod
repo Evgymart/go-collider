a7c1a3c9b906d3ce94f1d4c4e628cb1d29485f89
Evgymart <alignant@yandex.ru>
2026-02-02 21:36:27 +0300

    Basic database

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module collider
 
 go 1.25
+
+require (
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.11.1 // indirect
+)
---
+++ sql/init.sql
@@ -0,0 +1,26 @@
+create table users (
+    id uuid not null default gen_random_uuid() primary key,
+    name varchar(50) not null,
+    created_at timestamp(0) not null default (now() at time zone 'Europe/Moscow')
+);
+
+create table event_types (
+    id uuid not null default gen_random_uuid() primary key,
+    name varchar(255) not null unique
+);
+
+create table events (
+    id uuid not null default gen_random_uuid() primary key,
+    user_id uuid not null,
+    type_id uuid not null,
+    "timestamp" timestamp(0) not null default (now() at time zone 'Europe/Moscow'),
+    metadata jsonb not null,
+    constraint events_type_id_fkey foreign key (type_id)
+        references event_types (id) match simple
+        on update no action
+        on delete no action,
+    constraint events_user_id_fkey foreign key (user_id)
+        references users (id) match simple
+        on update no action
+        on delete no action
+);
